b025a2db9a987b53443b5bfbc41b80ad9c335f44
Can İlgu <45064650+Vitaee@users.noreply.github.com>
2020-02-05 23:12:48 +0300

    Update YemekSepet.py

--- YemekSepet.py
+++ YemekSepet.py
@@ -12,7 +12,7 @@ class Yemek:
 
     def yemekLogin(self):
         self.browser.get("https://www.yemeksepeti.com/kktc")
-        time.sleep(2)
+       
 
         usrnmInput = self.browser.find_element_by_xpath("//*[@id='UserName']")
         pswrdInput = self.browser.find_element_by_xpath("//*[@id='password']")
@@ -23,33 +23,33 @@ class Yemek:
 
         lgnBtn = self.browser.find_element_by_xpath("//*[@id='ys-fastlogin-button']")
         lgnBtn.click()
-        time.sleep(3)
+        
 
         ctySlct = self.browser.find_element_by_xpath("/html/body/header/div/div/div/div[2]/span/span[1]/span/span[2]")
         ctySlct.click() 
 
         Slcttown = self.browser.find_element_by_xpath("//*[@id='ys-areaSelector-droparea']/span/span/span[1]/input")
         Slcttown.send_keys(Keys.ENTER)
-        time.sleep(3)
+        
 
         srchBtn = self.browser.find_element_by_xpath("/html/body/header/div/div/div/div[4]/input")
         srchBtn.click()
         srchBtn.send_keys("burger")
         srchBtn.send_keys(Keys.ENTER)
-        time.sleep(2)
+        
 
         frstFood = self.browser.find_element_by_xpath("/html/body/div[3]/div/div[2]/div[2]/div[2]/div[1]/div[3]/div[1]/a/i")
         frstFood.click()
-        time.sleep(2)
+        
 
         foodFrst = self.browser.find_element_by_xpath("//*[@id='cboxLoadedContent']/div/div[2]/div/div[2]/button")
         foodFrst.click()
-        time.sleep(3)
+    
 
         
         confrmOrder = self.browser.find_element_by_xpath("//*[@id='basket-container']/div[2]/div/div[5]/button")
         confrmOrder.click()
-        time.sleep(3)
+       
 
         payMent = self.browser.find_element_by_xpath("/html/body/div[1]/div/div/div[8]/div/div/div/div/div[2]/div/div[2]/label/input")
         payMent.click()
