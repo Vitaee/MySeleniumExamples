3ec75b6bc30367929f7e43b386f978ae4adc9312
Can İlgu <45064650+Vitaee@users.noreply.github.com>
2020-02-05 23:22:13 +0300

    Update GithbLogin.py

--- GithbLogin.py
+++ GithbLogin.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 #https://selenium-python.readthedocs.io/
 from selenium.webdriver.common.keys import Keys
-from time import time
+
 
 gitname = "Account Name"
 gitpass = "password"
@@ -14,14 +14,14 @@ class Github:
 
     def singn(self):
         self.browser.get("https://github.com/login")
-        time.sleep(3)
+        
         gitnameInput = self.browser.find_element_by_xpath("//*[@id='login_field']")
         gitpassInput = self.browser.find_element_by_xpath("//*[@id='password']")
 
         gitnameInput.send_keys(self.gitname)
         gitpassInput.send_keys(self.gitpass)
         gitpassInput.send_keys(Keys.ENTER)
-        time.sleep(2)
+      
 
 githb = Github(gitname, gitpass)
 githb.singn()
