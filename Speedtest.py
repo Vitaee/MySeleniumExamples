034d91c802739cbd151b3f0e80eeb0244098b6aa
Can İlgu <45064650+Vitaee@users.noreply.github.com>
2020-02-05 23:22:30 +0300

    Update Speedtest.py

--- Speedtest.py
+++ Speedtest.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from time import time 
+
 
 class Speed:
     def __init__(self):
@@ -7,10 +7,10 @@ class Speed:
 
     def pressButon(self):
         self.browser.get("https://www.speedtest.net/")
-        time.sleep(2)
+       
         butonPress = self.browser.find_element_by_xpath("//*[@id='container']/div[2]/div/div/div/div[2]/div[3]/div[1]/a")
         butonPress.click()
-        time.sleep(2)
+      
 
 sped = Speed()
 sped.pressButon()
