b36e0c67f60a9cbd5b9e123273d00ac6888251ce
Can İlgu <45064650+Vitaee@users.noreply.github.com>
2020-02-08 17:23:55 +0300

    Update Speedtest.py

--- Speedtest.py
+++ Speedtest.py
@@ -1,16 +1,23 @@
 from selenium import webdriver
-
+#https://selenium-python.readthedocs.io/
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class Speed:
-    def __init__(self):
-        self.browser = webdriver.Chrome()
+        def __init__(self):
+            self.browser = webdriver.Chrome()
+            
 
-    def pressButon(self):
-        self.browser.get("https://www.speedtest.net/")
-       
-        butonPress = self.browser.find_element_by_xpath("//*[@id='container']/div[2]/div/div/div/div[2]/div[3]/div[1]/a")
-        butonPress.click()
-      
+        def pressButon(self):
+            self.browser.get("https://www.speedtest.net/")
+            self.browser.maximize_window()
+            try:
+                butonPress = WebDriverWait(self.browser,10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='container']/div[2]/div/div/div/div[2]/div[3]/div[1]/a")));
+                butonPress.click()
+
+            except:
+                print("Bir hata oluştu!")
 
 sped = Speed()
 sped.pressButon()
