570d3615e6d08201eb468abb08b9436bc57b57d5
Can İlgu <45064650+Vitaee@users.noreply.github.com>
2020-02-08 17:27:39 +0300

    Update GithbLogin.py

--- GithbLogin.py
+++ GithbLogin.py
@@ -2,26 +2,38 @@
 from selenium import webdriver
 #https://selenium-python.readthedocs.io/
 from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+gitname = ""
+gitpass = ""
 
-gitname = "Account Name"
-gitpass = "password"
 class Github:
-    def __init__(self,gitname,gitpass):
-        self.browser = webdriver.Chrome()
-        self.gitname = gitname
-        self.gitpass = gitpass
+        def __init__(self,gitname,gitpass):
+            driver = "/home/can/Downloads/chromedriver"
+            self.browser = webdriver.Chrome(driver)
+            self.gitname = gitname
+            self.gitpass = gitpass
 
-    def singn(self):
-        self.browser.get("https://github.com/login")
-        
-        gitnameInput = self.browser.find_element_by_xpath("//*[@id='login_field']")
-        gitpassInput = self.browser.find_element_by_xpath("//*[@id='password']")
+        def singn(self):
+            self.browser.get("https://github.com/login")
+            self.browser.maximize_window()
+            try:
+                gitnameInput = WebDriverWait(self.browser, 10).until(EC.visibility_of_element_located((By.XPATH,"//*[@id='login_field']")));
+                gitpassInput = self.browser.find_element_by_xpath("//*[@id='password']")
 
-        gitnameInput.send_keys(self.gitname)
-        gitpassInput.send_keys(self.gitpass)
-        gitpassInput.send_keys(Keys.ENTER)
-      
+                gitnameInput.send_keys(self.gitname)
+                gitpassInput.send_keys(self.gitpass)
+            except:
+                print("Bir sorun oldu!")
+
+            try:
+                gitLgn = WebDriverWait(self.browser, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='login']/form/div[3]/input[8]")));
+                gitLgn.click()
+            except:
+                print("Bilinmeyen bir hata!")
 
 githb = Github(gitname, gitpass)
 githb.singn()
