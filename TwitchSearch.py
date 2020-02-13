e19acf0fb9b1ba6101004d0e7cba014f84044159
Can İlgu <45064650+Vitaee@users.noreply.github.com>
2020-02-13 15:22:26 +0300

    Update TwitchSearch.py

--- TwitchSearch.py
+++ TwitchSearch.py
@@ -1,19 +1,41 @@
 from selenium import webdriver
+#https://selenium-python.readthedocs.io/
+from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-tw_ans = input("Enter a name of stramer: ").title()
+class Twitch: 
+	def __init__(self):
+		self.browser = webdriver.Chrome()
 
-class Twitch:
-    def __init__(self):
-            self.browser = webdriver.Chrome()
+	def openTwitch(self):
+		self.browser.get("https://www.twitch.tv/")
+		self.browser.maximize_window()
 
+		try:
+			barClick = WebDriverWait(self.browser,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div[2]/nav/div/div[2]/div/div')));
+			barClick.click()
+		except:
+			print("Element not clickable")
 
-    def searchTwtch(self):
-        self.browser.get("https://www.twitch.tv/" + tw_ans)
-        self.browser.maximize_window()
-      
-       
+	
 
-twitchSearch = Twitch()
-twitchSearch.searchTwtch()
+		try:
+			searchBar = WebDriverWait(self.browser, 10).until(EC.visibility_of_element_located((By.XPATH,'(//input[@type="search"])[1]')))
+			searchBar.send_keys("Jahrein")
+			searchBar.send_keys(Keys.ENTER)
+		except:
+			print("Element not writable")
+
+		try:
+			firstMan = WebDriverWait(self.browser,10).until(EC.element_to_be_clickable((By.XPATH,"//*[@id='root']/div/div[2]/div/main/div/div[3]/div/div/div/div/div[1]/div[1]/div[2]/div/div/div[2]/div/div/div/strong/a")));
+			firstMan.click()
+		except:
+			print("Can't clikc first streamer")
+
+twtch = Twitch()
+twtch.openTwitch()
 
